bb69c8d1fdaa4f758c2ee7ce4186ef7c4a38ffc6
Na1au <analaura-osilva@hotmail.com>
2023-05-30 01:00:32 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# COVID19_data_explorer
---
+++ android/app/src/main/kotlin/com/example/covid19_data_explorer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid19_data_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
